d5c1891e18464348d9fa5afd850724dd7eda3b44
zyz <zyz@U-QXWMKQDP-2304.local>
2026-07-29 11:17:48 +0800

    v7.6.2

--- app-v6-4.js
+++ app-v6-4.js
@@ -1,8 +1,8 @@
 (() => {
   "use strict";
 
-  window.TRAVEL_REVERIE_BUILD = "7.6.1-compact-map-card-title";
-  console.info("[Travel Reverie] build 7.6.1-compact-map-card-title loaded");
+  window.TRAVEL_REVERIE_BUILD = "7.6.2-watercolor-route-layer-fix";
+  console.info("[Travel Reverie] build 7.6.2-watercolor-route-layer-fix loaded");
 
   if ("caches" in window) {
     caches.keys().then(keys => {
